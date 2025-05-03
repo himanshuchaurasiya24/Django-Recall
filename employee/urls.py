399797913e23265a0e263d4e707e52a3051dfc0a
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2025-05-03 17:07:25 +0530

    pending nested serializers

--- employee/urls.py
+++ employee/urls.py
@@ -1,6 +1,19 @@
 from django.urls import include, path
 from .models import *
 from .views import *
+from . import views
+# for viewsets
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+
+# Method 4: Using ViewSets
+# router.register('employees',views.EmployeeViewSet,basename='Employee ViewSet')
+
+# Method 5: Using ModelViewSet
+router.register('employees',views.EmployeeModelViewSet,basename='Employee ModelViewSet')
+
+
+
 
 urlpatterns = [
     # Method 1: Using APIView class
@@ -12,7 +25,13 @@ urlpatterns = [
     # path('details/<int:pk>/', EmployeeRetrieveUpdateDeleteMixins.as_view(), name='employee-retrieve-update-delete'),
 
     # Method 3: Using generics
-    path('', EmployeeListCreateGenerics.as_view(), name='employee-list-create'),
-    path('details/<int:pk>/', EmployeeRetrieveUpdateDeleteGenerics.as_view(), name='employee-retrieve-update-delete'),
+    # path('', EmployeeListCreateGenerics.as_view(), name='employee-list-create'),
+    # path('details/<int:pk>/', EmployeeRetrieveUpdateDeleteGenerics.as_view(), name='employee-retrieve-update-delete'),
+
+    # Method 4: Using viewsets
+    # path('', include(router.urls), ),
+
+    # Method 5: Using ModelViewSet
+    path('', include(router.urls), )
 ]
 
--- employee/views.py
+++ employee/views.py
@@ -1,13 +1,13 @@
 from django.http import Http404
-from rest_framework import generics
 from .models import *
+from django.shortcuts import render, get_object_or_404
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import mixins
+from rest_framework import mixins, generics,viewsets
 
-# video at 02:36:53
+# video at 02:53:36
 
 # Method 1: Using APIView class
 
@@ -80,3 +80,41 @@ class EmployeeRetrieveUpdateDeleteGenerics(generics.RetrieveUpdateDestroyAPIView
     serializer_class = EmployeeSerializer
 
 #Method 4: Using viewsets
+
+class EmployeeViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Employee.objects.all()
+        serializer= EmployeeSerializer(queryset, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def create(self, request):
+        # queryset = Employee.objects.all() # --> Not required here...
+        serializer = EmployeeSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+    def retrieve(Self, request, pk=None):
+        # queryset = Employee.objects.get(pk=pk)
+        # serializers= EmployeeSerializer(queryset, many = True)
+        employee = get_object_or_404(Employee, pk =pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def update(self, request, pk= None):
+        queryset = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(queryset, data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def destroy(self, request, pk = None):
+        employee = get_object_or_404(Employee, pk= pk)
+        employee_name = employee.employee_name
+        employee.delete()
+        return Response({f'{employee_name} has been deleted!'}, status=status.HTTP_204_NO_CONTENT)
+
+# Method 5: Using ModelViewSet
+
+class EmployeeModelViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
