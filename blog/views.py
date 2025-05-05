6da7cbc0186f6252e7c824f15c7783644e6785ef
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2025-05-05 14:58:46 +0530

    ordering filter tutorial completed

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from blog.models import *
 from blog.serializers import *
+from rest_framework.filters import SearchFilter,OrderingFilter
 
 # Create your views here.
 
 class BlogModelViewSet(viewsets.ModelViewSet):
     queryset= Blog.objects.all()
     serializer_class= BlogSerializer
+    filter_backends= [SearchFilter, OrderingFilter]
+    # filter_backends= [ OrderingFilter]
+    search_fields= ['blog_title', 'blog_body']  # Specify the fields to search on
+    # search_fields= ['^blog_title', 'blog_body']  # The ^ symbol is to search the given word in the sentence which start with it, Specify the fields to search on
+    ordering_fields= ['blog_title', 'blog_body','id']  # Specify the fields to order by
     # lookup_field = 'blog_title'  # Specify the lookup field for the Blog model
 class CommentModelViewSet(viewsets.ModelViewSet):
     queryset= Comment.objects.all()
     serializer_class= CommentSerializer
+    filter_backends= [SearchFilter]
     # lookup_field = 'blog_id'  # Specify the lookup field for the Comment model
 
 # class BlogListCreateView(generics.ListCreateAPIView):
